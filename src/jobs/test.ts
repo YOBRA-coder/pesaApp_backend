d3422b153594c097b08881aa23bcefdaaafb0b56
YOBRA-coder <56080569+YOBRA-coder@users.noreply.github.com>
2026-04-29 05:36:03 +0300

    Remove unused axios call in test.ts

--- src/jobs/test.ts
+++ src/jobs/test.ts
@@ -1,7 +1,1 @@
-const res = await axios.get('https://api.the-odds-api.com/v4/sports/soccer_epl/odds', {
-  params: { apiKey: process.env.ODDS_API_KEY, regions: 'eu', markets: 'h2h,totals' }
-});
 
-if(res){
-    
-}
